caa3a05d4386ddb1f4d2a40f472044e663190bdb
RobertGRaleigh <robertgraleigh@gmail.com>
2017-05-11 12:22:07 -0700

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// webpack.config.js
+(function () {
+	module.exports = {
+		
+	}
+}());
+
+/*
+	OTHER WAYS OF CONFIGURING THE WEBPACK CONFIG FILE
+
+	module.exports = {
+
+	}
+
+	var config = function () {
+
+	}
+
+	module.exports = config;
+*?
